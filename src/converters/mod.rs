66d98f1cf46684d070a6d645b926c10366582b67
Jacob Sapoznikow <500074@bsd48.org>
2022-06-16 20:59:19 +0000

    Fixes and async!

--- src/converters/mod.rs
+++ src/converters/mod.rs
@@ -1,1 +1,2 @@
-pub mod vanilla;
+pub mod faber;
+pub mod vanel;
--- src/lib.rs
+++ src/lib.rs
@@ -3,68 +3,43 @@ pub use forgic::*;
 pub use quantum_mc::*;
 pub use vanel::*;
 
+pub mod converters;
+pub mod util;
+
+use neon::prelude::{ModuleContext, NeonResult};
+
+#[neon::main]
+pub fn main(mut ctx: ModuleContext) -> NeonResult<()> {
+    ctx.export_function("getFabricVersions", converters::faber::get_fabric_versions)?;
+    return Ok(());
+}
+
 pub mod tests {
-    pub use std::{
-        fs::{create_dir, File},
-        io::Write,
-    };
-
-    pub use faber::*;
-    pub use forgic::*;
-    pub use futures::executor::block_on;
-    pub use quantum_mc::*;
-    pub use vanel::*;
-
-    pub fn test_faber() -> faber::MavenVersionResult {
-        let run = get_fabric_versions();
-        let out = block_on(run);
+    #[test]
+    fn test_faber() {
+        let run = faber::get_fabric_versions();
+        let out = futures::executor::block_on(run);
         println!("{:?}", out);
-        return out;
-    }
-
-    pub fn test_forgic() -> forgic::MavenVersionResult {
-        let run = get_forge_versions();
-        let out = block_on(run);
-        println!("{:?}", out);
-        return out;
-    }
-
-    pub fn test_quantum() -> quantum_mc::MavenVersionResult {
-        let run = get_quilt_versions();
-        let out = block_on(run);
-        println!("{:?}", out);
-        return out;
-    }
-
-    pub fn test_vanel() -> MinecraftVersionResult {
-        let run = get_minecraft_versions();
-        let out = block_on(run);
-        println!("{:?}", out);
-        return out;
     }
 
     #[test]
-    pub fn test_all() {
-        let faber_test = test_faber();
-        let forgic_test = test_forgic();
-        let quantum_test = test_quantum();
-        let vanel_test = test_vanel();
+    fn test_forgic() {
+        let run = forgic::get_forge_versions();
+        let out = futures::executor::block_on(run);
+        println!("{:?}", out);
+    }
 
-        let faber_out = serde_json::to_string_pretty(&faber_test).unwrap();
-        let forgic_out = serde_json::to_string_pretty(&forgic_test).unwrap();
-        let quantum_out = serde_json::to_string_pretty(&quantum_test).unwrap();
-        let vanel_out = serde_json::to_string_pretty(&vanel_test).unwrap();
+    #[test]
+    fn test_quantum() {
+        let run = quantum_mc::get_quilt_versions();
+        let out = futures::executor::block_on(run);
+        println!("{:?}", out);
+    }
 
-        create_dir("tests").unwrap();
-
-        let mut faber_file = File::create("tests/faber.json").unwrap();
-        let mut forgic_file = File::create("tests/forgic.json").unwrap();
-        let mut quantum_file = File::create("tests/quantum.json").unwrap();
-        let mut vanel_file = File::create("tests/vanel.json").unwrap();
-
-        faber_file.write_all(faber_out.as_bytes()).unwrap();
-        forgic_file.write_all(forgic_out.as_bytes()).unwrap();
-        quantum_file.write_all(quantum_out.as_bytes()).unwrap();
-        vanel_file.write_all(vanel_out.as_bytes()).unwrap();
+    #[test]
+    fn test_vanel() {
+        let run = vanel::get_minecraft_versions();
+        let out = futures::executor::block_on(run);
+        println!("{:?}", out);
     }
 }
