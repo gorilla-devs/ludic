32a62a16d80636e061e205c9b3c6660fbcb79bce
Jacob Sapoznikow <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-15 19:41:13 -0700

    Add code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
+pub use faber::*;
+pub use forgic::*;
+pub use quantum::*;
+pub use vanel::*;
+
+pub mod tests {
+    pub use faber::*;
+    pub use forgic::*;
+    pub use futures::executor::block_on;
+    pub use quantum::*;
+    pub use vanel::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    pub fn test_faber() {
+        let run = get_fabric_versions();
+        block_on(run);
+    }
+
+    #[test]
+    pub fn test_forgic() {
+        let run = get_forge_versions();
+        block_on(run);
+    }
+
+    #[test]
+    pub fn test_quantum() {
+        let run = get_quilt_versions();
+        block_on(run);
     }
 }
